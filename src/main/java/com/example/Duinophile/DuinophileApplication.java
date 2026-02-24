bfeb42f624d45fb5f37a0bc2797086f733b42b1b
Stork <it24103874@my.sliit.lk>
2026-02-24 22:48:27 +0530

    first commit

---
+++ src/main/java/com/example/Duinophile/DuinophileApplication.java
@@ -0,0 +1,13 @@
+package com.example.Duinophile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuinophileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuinophileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Duinophile/DuinophileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Duinophile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuinophileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
