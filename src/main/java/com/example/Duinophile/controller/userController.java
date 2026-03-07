b96238bd22f75a90ec7ffc7e2874b950aaa76900
Stork <it24103874@my.sliit.lk>
2026-03-07 09:24:27 +0530

    Commiting Course management

---
+++ src/main/java/com/example/Duinophile/controller/userController.java
@@ -0,0 +1,22 @@
+package com.example.Duinophile.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class userController {
+    @GetMapping("/")
+    public String indexLoad(){
+        return "IndexPage";
+    }
+
+    @PostMapping("/users/login")
+    public String login(@RequestParam String username, @RequestParam String password){
+        if (username.equals("Admin") && password.equals("Password")){
+            return "courses";
+        }
+        return "IndexPage";
+    }
+}
